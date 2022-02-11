fa58d3293c976279d3709246dbae285c208983de
Andrei Voinea <8058187+andreivdev@users.noreply.github.com>
2022-02-11 20:10:59 +0200

    works

---
+++ Brewfile
@@ -0,0 +1,30 @@
+# tap various cask repositories before installing
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/cask-fonts"
+tap "homebrew/cask-versions"
+tap "homebrew/core"
+tap "homebrew/cask-drivers"
+
+brew "fnm"
+brew "mas"
+
+cask "1password"
+cask "ledger-live"
+cask "brave-browser"
+cask "discord"
+cask "github"
+cask "visual-studio-code"
+cask "vlc"
+cask "whatsapp"
+cask "monitorcontrol"
+cask "blackhole-2ch"
+cask "obs"
+cask "flutter"
+
+mas "Keynote", id: 409183694
+mas "Numbers", id: 409203825
+mas "Pages", id: 409201541
+mas "The Unarchiver", id: 425424353
+mas "Xcode", id: 497799835
+mas "Magnet", id: 441258766
---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Ask for administrator password upfront
+sudo -v
+
+# sudo keep-alive
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install xcode developer tools
+xcode-select --install
+sudo xcode-select -switch /Library/Developer/CommandLineTools
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/andrei/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Install Brewfile contents
+brew bundle
+
+sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
+sudo xcodebuild -license accept
+
+# node
+
+echo 'eval "$(fnm env)"' >> /Users/andrei/.zprofile
+eval "$(fnm env)"
+fnm install 16.14.0
+fnm use 16.14.0
+
+chmod +x ./.macos
+./.macos
